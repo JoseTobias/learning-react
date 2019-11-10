aa1e39213946a14fa4c3fe1df23a59ff9a86f9e5
JoseTobias <tobiassouza0@gmail.com>
2019-11-10 09:49:42 -0300

    Project to-do

---
+++ to-do-app/front/src/components/todo/TodoForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Grid from '../template/Grid'
+import IconButton from '../template/IconButton'
+
+export default props => (
+    <div role='form' className='todoForm row'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control'
+            placeholder='Cadastre uma tarefa'
+            onChange={props.handleChange}
+            value={props.description}/>
+        </Grid>
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd} />
+            <IconButton style='info' icon='search'
+            onClick={props.handleSearch} />
+            <IconButton style='default' icon='times'
+            onClick={props.handleClear}></IconButton>
+        </Grid>
+    </div>
+)
