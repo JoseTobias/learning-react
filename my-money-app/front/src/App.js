6d05e2bd983ac740f7d2076d3030fa86fcbe2f03
JoseTobias <tobiassouza0@gmail.com>
2020-02-07 09:27:28 -0300

    sidebar

--- my-money-app/front/src/App.js
+++ my-money-app/front/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Header from '../src/common/template/header'
+import SideBar from '../src/common/template/sideBar'
 
 export default (props) => (
-  <div>
+  <div className='app'>
     <Header />
+    <SideBar />
   </div>
 )
---
+++ my-money-app/front/src/common/template/sideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Menu from './menu'
+
+export default props => (
+    <aside className='main-sidebar col-3'>
+      <section className='sidebar'>
+        <Menu />
+      </section>
+    </aside>
+)
