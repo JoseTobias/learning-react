f3b444768b9f5fe9b8ec727d591d97a057ff1f4e
JoseTobias <tobiassouza0@gmail.com>
2020-02-07 09:01:05 -0300

    adding header

---
+++ my-money-app/front/src/common/template/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+  <header className='main-header'>
+      <a href="/" className='logo'>
+        <span className='logo-lg'>
+        <i class="far fa-money-bill-alt"></i>
+          <b> My</b> Money
+        </span>
+      </a>
+  </header>
+)
--- my-money-app/front/src/index.js
+++ my-money-app/front/src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import './common/template/custom.css'
+
 ReactDOM.render(<App />, document.getElementById('root'));
