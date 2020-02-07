1a48e73f4b15d5767b4e013464f426ac2c23af60
JoseTobias <tobiassouza0@gmail.com>
2020-02-07 11:15:17 -0300

    footer

--- my-money-app/front/src/App.js
+++ my-money-app/front/src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 
 import Header from '../src/common/template/header'
 import SideBar from '../src/common/template/sideBar'
+import Footer from '../src/common/template/footer'
 
 export default (props) => (
   <div className='app'>
     <Header />
-    <SideBar />
+    <div className='main'>
+      <SideBar />
+      <div className='p-2'>
+        <h1>Conteúdo</h1>
+      </div>
+    </div>
+    <Footer />
   </div>
 )
---
+++ my-money-app/front/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <div className='container'>
+            <strong>
+                Copyright <i class="far fa-copyright"></i> 2020
+                <a href="https://josetobias.github.io/" target='_blank'> tobias</a>.
+            </strong>
+        </div>
+    </footer>
+)
