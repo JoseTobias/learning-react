38df6e6a05f1634920dfa62758dd28d9c78bed32
JoseTobias <tobiassouza0@gmail.com>
2020-02-11 10:42:04 -0300

    adding redux

--- my-money-app/front/src/index.js
+++ my-money-app/front/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import promisse from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
+import reducers from './reducers'
+
+
 import './index.css';
 import App from './App';
 
@@ -7,4 +16,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './common/template/custom.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
